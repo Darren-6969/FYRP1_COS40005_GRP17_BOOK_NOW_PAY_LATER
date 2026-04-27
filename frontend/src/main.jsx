3621bfd99ca96dc3491eef7e1dffaa2a6e878e4e
Darren-6969 <102778736@students.swinburne.edu.my>
2026-04-27 23:34:57 +0800

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,7 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+
 import "./assets/styles/global.css";
+import "./assets/styles/components.css";
+import "./assets/styles/auth.css";
+import "./assets/styles/master.css";
+import "./assets/styles/customer.css";
+import "./assets/styles/operator.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
